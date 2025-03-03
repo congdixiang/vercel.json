b96b967b3562eed75d5a377b406d5c643c972582
congdixiang <csx.006@timg.cc>
2025-03-03 12:21:14 +0800

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function (req, res) {
   const host = req.headers.host.split('.')[0];
-  const destinationURL = `https://${host}.906009898.xyz:368${req.url}`;
+  const destinationURL = `https://${host}.timg.cc:368${req.url}`;
   
   res.writeHead(301, { Location: destinationURL });
   res.end();
