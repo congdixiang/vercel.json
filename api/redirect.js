754c3f02bb560d71d6502cc042d71f6cccba8239
congdixiang <csx.006@timg.cc>
2025-03-03 13:13:13 +0800

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,13 @@
-export default function (req, res) {
-  const host = req.headers.host.split('.')[0];
-  const destinationURL = `https://${host}.906009898.xyz:368${req.url}`;
+const http = require('http');
+
+// 创建 HTTP 服务器
+http.createServer((req, res) => {
+  // 设置重定向的目标 URL
+  const destinationURL = `https://${req.headers.host}${req.url}`;
   
+  // 设置 301 状态码和重定向头信息
   res.writeHead(301, { Location: destinationURL });
   res.end();
-}
+}).listen(80, () => {
+  console.log('Server running at http://abc.906009898.xyz/');
+});
